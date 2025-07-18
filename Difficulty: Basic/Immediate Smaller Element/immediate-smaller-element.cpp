7047cd0c5103c6977294be1ead8e140cbe2bf5e2
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2025-07-18 22:59:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Immediate Smaller Element/immediate-smaller-element.cpp
@@ -0,0 +1,17 @@
+// User function template for C++
+class Solution {
+  public:
+    void immediateSmaller(vector<int>& arr) {
+        int n = arr.size();
+        stack<int> st;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                arr[i] = arr[i+1];
+            }
+            else{
+                arr[i] = -1;
+            }
+        }
+        arr[n-1] = -1;
+    }
+};
