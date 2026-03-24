044870b234d1d69d3c9a2e913c1e0b42bc9043d7
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2026-03-24 22:05:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -1,14 +1,12 @@
 class Solution {
   public:
-    // Function to find the minimum number of platforms required at the
-    // railway station such that no train waits.
-    int findPlatform(vector<int>& arr, vector<int>& dep) {
+    int minPlatform(vector<int>& arr, vector<int>& dep) {
         int n = arr.size();
         sort(arr.begin(),arr.end());
         sort(dep.begin(),dep.end());
         int ans = 1;
-        int count=1;
-        int i=1,j=0;
+        int count = 1;
+        int i = 1,j = 0;
         while(i<n && j<n){
             if(arr[i]<=dep[j]){
                 i++;
@@ -19,6 +17,7 @@ class Solution {
                 count--;
             }
             ans = max(ans,count);
-        }return ans;
+        }
+        return ans;
     }
 };
