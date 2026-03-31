4071748857cbf3df91b883476ae33862f5758889
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2026-03-31 16:48:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
+++ Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
@@ -1,30 +1,15 @@
-// User function Template for C++
-
 class Solution {
   public:
-    void coins(vector<int>& curr,int i,int money,vector<int>& ans){
-        if(money==0){
-            return;
-        }
-        if(curr[i]<=money){
-            ans.push_back(curr[i]);
-            coins(curr,i,money-curr[i],ans);
-        }else{
-            coins(curr,i-1,money,ans);
-        }
-        return;
+    int loop(vector<int>& temp,int n,int i){
+        if(n==0) return 0;
+        if(i==0) return n;
+        int x;
+        if(n>=temp[i]) x = 1+loop(temp,n-temp[i],i);
+        else x = loop(temp,n,i-1);
+        return x;
     }
-    vector<int> minPartition(int N) {
-        
-        vector<int> curr = { 1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
-        vector<int> ans;
-        for(int i=9;i>=0;i--){
-            if(N>=curr[i]){
-                ans.push_back(curr[i]);
-                coins(curr,i,N-curr[i],ans);
-                break;
-            }
-        }
-        return ans;
+    int findMin(int n) {
+        vector<int> temp = {1,2,5,10};
+        return loop(temp,n,3);
     }
 };
